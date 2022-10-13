37493244c0248f15a34340f91ae17568564eaa2d
Chris Lai <chrisyflai@gmail.com>
2022-10-14 00:01:19 +0800

    New FR1

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-/// view engine setup
-// My new FR1
+// view engine setup
+// My New FR1
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
